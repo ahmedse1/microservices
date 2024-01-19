3f621bbaa27a11d56f010c5f48153e99ec5ef580
Ahmed Hassan <Ahmed.Hassan@gls-itservices.com>
2024-01-19 10:51:18 +0100

    initial commit

---
+++ src/main/java/com/example/practice/controllers/AccountsController.java
@@ -0,0 +1,14 @@
+package com.example.practice.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayhello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
